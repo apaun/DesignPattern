2e95f2c473f7d7f4457714aa26d209de7f3062f0
apaun <abhijeet.paunikar@gmail.com>
2019-07-13 14:47:04 +0530

    Factory

---
+++ DesignPattern/FactoryMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new ConcreteFactoryA();
+            Product _product = factory.GetProduct();
+            Console.WriteLine(_product.ToString());
+
+            factory = new ConcreteFactoryB();
+            _product = factory.GetProduct();
+            Console.WriteLine(_product.ToString());
+
+        }
+    }
+}
